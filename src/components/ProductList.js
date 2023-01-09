3848a4da1a01be2c1d386d072c91d0510373bdfc
chuongduong <duongthuychuong@gmail.com>
2023-01-09 22:48:03 +0700

    product page list and grid view

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -4,7 +4,16 @@ import GridView from "./GridView";
 import ListView from "./ListView";
 
 const ProductList = () => {
-  const { filtered_products: products } = useFilterContext();
+  const { filtered_products: products, gridview } = useFilterContext();
+
+  if (products.length < 1) {
+    <h5 style={{ textTransform: "none" }}>
+      Sorry, no poructs matched your search...
+    </h5>;
+  }
+  if (gridview === false) {
+    return <ListView products={products} />;
+  }
   return <GridView products={products}></GridView>;
 };
 
--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -15,6 +15,7 @@ import { useProductsContext } from "./products_context";
 const initialState = {
   filtered_products: [],
   all_products: [],
+  gridview: false,
 };
 
 const FilterContext = React.createContext();
