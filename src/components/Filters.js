0072de387a77a34349c3fbbce6705322d7b84e8a
chuongduong <duongthuychuong@gmail.com>
2023-01-11 22:55:32 +0700

    add key to map item

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -96,6 +96,7 @@ const Filters = () => {
                 if (c === "all") {
                   return (
                     <button
+                      key={index}
                       name="color"
                       onClick={updateFilter}
                       data-color="all"
