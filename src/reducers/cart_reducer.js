0266623cf88e0b31e505fb0469927749b736eea7
duongthuychuong <duongthuychuong@gmail.com>
2023-02-27 17:47:17 +0700

    new item

--- src/reducers/cart_reducer.js
+++ src/reducers/cart_reducer.js
@@ -4,11 +4,31 @@ import {
   COUNT_CART_TOTALS,
   REMOVE_CART_ITEM,
   TOGGLE_CART_ITEM_AMOUNT,
-} from '../actions'
+} from "../actions";
 
 const cart_reducer = (state, action) => {
-  return state
-  throw new Error(`No Matching "${action.type}" - action type`)
-}
+  if (action.type === ADD_TO_CART) {
+    const { id, color, amount, product } = action.payload;
+    const findedItem = state.cart.find((item) => id + color === item.id);
+    if (findedItem) {
+    } else {
+      const newItem = {
+        id: id + color,
+        name: product.name,
+        color,
+        amount,
+        image: product.images[0].url,
+        price: product.price,
+        max: product.stock,
+      };
+      return {
+        ...state,
+        cart: [...state.cart, newItem],
+      };
+    }
+  }
+  return state;
+  throw new Error(`No Matching "${action.type}" - action type`);
+};
 
-export default cart_reducer
+export default cart_reducer;
